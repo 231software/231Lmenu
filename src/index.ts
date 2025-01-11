988a26a22c1abda1a2c2dd815f072178d46f6a17
Minimouse <473147334@qq.com>
2025-01-11 15:53:57 +0800

    修复所有问题，该版本已上传服务器

--- src/index.ts
+++ src/index.ts
@@ -20,49 +20,6 @@ import {Logger,InitEvent, JsonFile,
 } from "../lib/index.js";
 import {data_path} from "../lib/plugin_info.js"
 const globalConf = new JsonFile(data_path+"/config.json");
-/*
-class MenuConfig{
-    path:string
-    content:string
-    constructor(path){
-        this.path=path
-        this.init()
-        this.reload()
-    }
-    init(){
-        const lastDir=new Directory(this.path)
-        const menuFileName=lastDir.folders[lastDir.folders.length-1]
-        lastDir.folders.push("..")
-        if(File.ls(lastDir.toString()).includes(menuFileName))return
-        File.forceWrite(lastDir.toString(),JSON.stringify(
-            [
-                { 
-                    name: "main", 
-                    permission: 0, 
-                    title: "", 
-                    caption: "", 
-                    contents: [
-                        { 
-                            name: "", 
-                            title: "", 
-                            action: [
-                                {
-                                    type:"tell",
-                                    value:""
-                                }
-                            ]
-                        }
-                    ] 
-                }
-            ]
-        ))
-    }
-    reload(){
-        this.content=File.read(this.path)
-    }
-
-}
-    */
 const menucontents = new JsonFile(data_path+"/menu.json");
 function tellExecutor(executor:CommandExecutor,msg:string){
     switch(executor.type){
@@ -83,6 +40,7 @@ function parseButtonActionType(str:string):ButtonActionType{
     switch(str){
         case "tell":return ButtonActionType.TELL;
         case "fm":return ButtonActionType.FORM;
+        case "plcmd":
         case "plcmdex":return ButtonActionType.PLCMD;
         case "cmd":return ButtonActionType.CMD
         case "condcmd":return ButtonActionType.CONDCMD
@@ -153,6 +111,18 @@ menucontents.init("menus",[
         ] 
     }
 ])
+let menusConf:any[]=menucontents.get("menus")
+function reloadMenus(){
+    menucontents.reload()
+    menusConf=menucontents.get("menus")
+}
+function getMenu(name:string):MenuConfig{
+    for(let menuConf of menusConf){
+        const menu=menuConfFromConfig(menuConf)                    
+        if (menu.name == name) return menu
+    }
+    throw new Error("找不到菜单"+name)
+}
 
 const maincmd=new Command(
     "231l",[
@@ -165,28 +135,19 @@ const maincmd=new Command(
         if(result.executor.type==CommandExecutorType.Player){
             const player:Player=result.executor.object
             if (result.params.get("spawn")?.value == "spawn") {
-                player.teleport(new Location(520, 69.5, 118,new Dimension(FMPDefaultDimension.Overworld)));
+                player.teleport(Location.new(520, 69.5, 118,new Dimension(FMPDefaultDimension.Overworld)));
                 player.tell(`§a已将您传送至起始点`);
             }  		
             else if (result.params.get("shop")?.value == "shop") {
                 //shopForm(origin.player);
                 tellExecutor(result.executor,"请为此指令设置跳转到专门商店菜单的功能")
             }
-            else {
-                const menusConf:any[]=menucontents.get("menus")
-                for(let menuConf of menusConf){
-                    const menu=menuConfFromConfig(menuConf)                    
-                    if (menu.name == "main") {
-                        form(menu,player);
-                    }
-                }
-            }          
+            else form(getMenu("main"),player);
         }
 		else if (result.params.get("reload")?.value == "reload") {
 			if (result.params.get("reloadtype")?.value == "menu") {
-                tellExecutor(result.executor,"请完善此功能")
-				//playerreloadmenu(origin.player);
-				
+				reloadMenus();
+				tellExecutor(result.executor,"菜单重载完成")
 			}
 			if (result.params.get("reloadtype")?.value == "shop") {
                 tellExecutor(result.executor,"请完善此功能")
@@ -204,128 +165,22 @@ const maincmd=new Command(
 
 function form(menu:MenuConfig,playerOrSession:Player|SimpleFormSession) {
     const player:Player=playerOrSession instanceof Player?playerOrSession:playerOrSession.player
+    let currentSession:SimpleFormSession
     const buttons:SimpleFormButton[]=[]
-	menu.contents.forEach((button, index) => buttons.push(new SimpleFormButton(button.title,button.title,session=>{
+	menu.contents.forEach((button) => buttons.push(new SimpleFormButton(button.title,button.title,session=>{
+        /**这些动作中命令是否仍然在成功执行。如果有一条命令执行失败，此处会被设置为false */
         let cmdsuccess = true;
         button.actions.forEach(action=> {
             switch (action.type) {
                 case ButtonActionType.TELL: player.tell(action.value); break;
-                //case ButtonActionType.PLCMD: mc.runcmdEx(`execute as ${player.realName} at @s run ${action.value}`); break;
-                case ButtonActionType.PLCMD: throw new Error("满月平台需要先新增玩家执行命令api")//player.runcmd(action.value); break;
+                case ButtonActionType.PLCMD: player.runCmd(action.value); break;
                 case ButtonActionType.CMD: cmdsuccess = runcmd(action.value).success ; break;
-                case ButtonActionType.CONDCMD:throw new Error("条件cmd")//if (cmdsuccess) { cmdsuccess = mc.runcmdEx(action.value).success; } break;
-                case ButtonActionType.FORM: {
-                    const menusConf:any[]=menucontents.get("menus")
-                    for(let menuConf of menusConf){
-                        const menu=menuConfFromConfig(menuConf)  
-                        if (menu.name == action.value ) form(menu, playerOrSession);
-                    }
-                    break;
-                }
+                //当之前有命令执行失败时，则不执行该命令
+                case ButtonActionType.CONDCMD:if (cmdsuccess) cmdsuccess = runcmd(action.value).success;  break;
+                case ButtonActionType.FORM: form(getMenu(action.value), currentSession);break;
             }
         })
     })))
-    new SimpleFormSession(new SimpleForm(menu.title,menu.caption),playerOrSession).send()
-}
-function aNewForm(player){
-    let fm = mc.newSimpleForm()
-    fm.setTitle("231L")
-    fm.setContent("")
-    fm.addButton('传送')
-    fm.addButton('签到')
-	fm.addButton('个人信息')
-	fm.addButton('商店')
-    player.sendForm(fm, function (player,id) {
-		switch(id){
-			case 0:tpform(player);break;
-			case 1:player.runcmd(`signin`);break;
-			case 2:information(player);break;
-			case 3:shopForm(player);break;
-		}
-    })
-}
-//个人信息
-function information(player){
-	infofm=mc.newSimpleForm()
-	infofm.setTitle(`${player.realName}的个人信息`)
-	infofm.setContent(`您当前拥有${mc.getScoreObjective("integral").getScore(player)}积分`)
-	player.sendForm(infofm,function(player,id){})
-}
-//传送ui
-function tpform(player){
-	tpfm=mc.newSimpleForm()
-	tpfm.addButton('主城')
-	tpfm.addButton('起始点')
-	player.sendForm(tpfm,function(player,id){
-		if(id==0){
-			player.teleport(-7068.5, 70, 1994.5, 0);
-			player.tell(`§a已将您传送至主城`);			
-		}
-		if(id==1){
-			player.teleport(520, 69.5, 118, 0);
-			player.tell(`§a已将您传送至起始点`);			
-		}		
-	})
-}
-function playerreloadmenu(player) {
-	if (menucontents.reload()) {
-		player.tell(`§a重载完成，请留意后台报错信息`);
-	} else {
-		player.tell(`§a无法重载菜单内容`);
-	}
-}
-//商店
-const shopContentsfile=new JsonConfigFile("plugins\\231Essentials\\features\\shop\\contents.json");
-let shopContents = JSON.parse(shopContentsfile.read());
-//重载
-
-function shopReload(player) {
-	if(shopContentsfile.reload()){
-		player.tell(`§a重载完成，请留意后台报错信息`);
-	}else{
-		player.tell(`§a无法重载商店内容`);
-	}
-	if(shopContentsfile.reload()){
-		player.tell(`§a重载完成，请留意后台报错信息`);
-	}else{
-		player.tell(`§a无法重载商店内容`);
-	}
-	shopContents = JSON.parse(shopContentsfile.read());
-}
-/*mc.regPlayerCmd(`231e shop reload`, `重载商店内容`, (player) => {
-
-}, 1)*/
-//商店ui
-function shopForm(player){
-	let shopfm=mc.newSimpleForm()
-	for(i=0;i<shopContents.shopsorts.length;i++){
-		shopfm.addButton(shopContents.shopsorts[i].sort)
-	}
-
-	player.sendForm(shopfm, function (player,id) {
-		if(id==null){
-			player.runcmdEx("231l")
-			return;
-		}	
-		shopForm1(player,id);
-	})
-}
-function shopForm1(player,sort){
-	let shopfm1=mc.newSimpleForm()
-	for(i=0;i<shopContents.shopsorts[sort].contents.length;i++){
-		shopfm1.addButton(`${shopContents.shopsorts[sort].contents[i].name}:${shopContents.shopsorts[sort].contents[i].price}积分`)
-	}
-	player.sendForm(shopfm1, function (player,id) {
-		if(id==null){
-			shopForm(player)
-			return;
-		}		
-		//纯指令购买
-		mc.runcmdEx(`tag @p[name="${player.realName}",scores={integral=${shopContents.shopsorts[sort].contents[id].price}..}] add affordable`);
-		mc.runcmdEx(`scoreboard players remove @a[name="${player.realName}",tag=affordable] integral ${shopContents.shopsorts[sort].contents[id].price}`);
-		mc.runcmdEx(`give @a[name="${player.realName}",tag=affordable] ${shopContents.shopsorts[sort].contents[id].type}`);
-		mc.runcmdEx(`tell @a[name="${player.realName}",tag=affordable] 购买成功`);
-		mc.runcmdEx(`tell @a[name="${player.realName}",tag=!affordable] 您的余额不足`);
-		mc.runcmdEx(`tag @a[name="${player.realName}",tag=affordable] remove affordable`);
-	})
+    currentSession=new SimpleFormSession(new SimpleForm(menu.title,menu.caption,buttons),playerOrSession)
+    currentSession.send()
 }
